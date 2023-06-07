31d7f742fbd8ffa14ca4723cb642a0498aca6ffe
samisaafi <127246541+samisaafi@users.noreply.github.com>
2023-06-07 19:05:01 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import UserList from './UserList';
 const App = () => {
   return (
     <div className="App">
-      <UserList />
+      <UserList style={{marginTop: "50px", boxSizing:"border-box", borderBlockStyle:"double", backgroundImage: "linear-gradient(yellow,lightblue)", padding:"30px",fontsize: "20px", color:"#4a54f1", textalign:"center"}}/>
     </div>
   );
 };
